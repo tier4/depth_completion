fix: update default option for binned scores calculation
- Changed the default value of the `--calc-binned-scores` option from False to True, enabling binned scores computation by default.
- This adjustment aims to enhance the analysis process by ensuring binned scores are calculated unless explicitly disabled.

analyze.py
@@ -52,7 +52,7 @@ torch.set_float32_matmul_precision("high")  # NOTE: Optimize fp32 arithmetic
 @click.option(
     "--calc-binned-scores",
     type=bool,
-    default=False,
+    default=True,
     help="Whether to compute binned scores.",
     show_default=True,
 )